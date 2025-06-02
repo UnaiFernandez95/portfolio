4c6d5bcfa4337f32830dd6c170486b73c35451a7
UnaiFernandez95 <unaifernandez@outlook.com>
2025-06-02 23:47:12 +1000

    relative path img profile photo

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import profilePhoto from "../assets/profilePhoto.jpg";
+import profilePhoto from "../../assets/profilePhoto.jpg";
 import "./nav.css";
 
 const Nav = () => {
